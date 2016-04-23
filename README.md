c6f09c655bde9bdf0e389c31ac37f40d444bb9ba
Martin Iwanowski <me@fl0w.io>
2016-04-23 12:31:07 +0200

    Solidifying w/ updated README and a test suite

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-## jwt-then
+# jwt-then
 
 Hack'n'slash promisify.
-This isn't an honest package, and you should treat it with suspicion.
+
+## API
+
+Testing on `node 4+`
+Implements two methods only, `verify` and `sign`.
+
+### jwt.sign(payload, key, [options])
+
+Sign a token with key, with options defaulting to `{ algorithm: 'HS256' }`.
+
+```js
+jwt.sign({ username: 'fl0w' }, 'secret-dev-key')
+  .then(token => console.log(token))
+```
+
+Marty McFly's: ES7 (or using Present MacFly's babel.js)
+
+```js
+const token = await jwt.sign({ username: 'fl0w', 'secret-dev-key'})
+console.log(token)
+```
+
+### jwt.verify(token, key, [options])
+
+Verify a token, with options defaulting to `{ algorithm: 'HS256' }`.
+
+```js
+jwt.verify(token, 'secret-dev-key')
+  .then(function (token) { console.log(token) })
+```
+
+Marty McFly's: ES7 (or using Present MacFly's babel.js)
+
+```js
+const token = await jwt.verify({ username: 'fl0w', 'secret-dev-key'})
+console.log(token)
+```
---
+++ test/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const assert = require('assert')
+const jwt = require('..')
+
+describe('jwt', function () {
+  describe('.sign', function () {
+    it ('(payload, key)', function () {
+      return jwt.sign({ id: 123456 }, 'dev-secret').then(function (token) {
+        assert(typeof token === 'string')
+      })
+    })
+  })
+
+  describe('.verify', function () {
+    const token = [
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9',
+      '.eyJpZCI6MTIzNDU2LCJpYXQiOjE0NjE0MDYzOTV9',
+      '.QZge-iFTDWEuOoGtdhwg6e7UzCzuAn831BiAVEzaOU4'
+    ].join('')
+
+    it ('(token, key) successfully', function () {
+      return jwt.verify(token, 'dev-secret').then(function (token) {
+        assert(token)
+        assert(token.id === 123456)
+      })
+    })
+
+    it ('(token, key) w/ wrong secret', function () {
+      return jwt.verify(token, 'wrong-secret')
+        .catch(function (err) {
+          assert(err.name === 'JsonWebTokenError')
+        })
+    })
+  })
+})
